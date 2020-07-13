8cb297643dce6c687cc24207fbda38be4ab05f9b
CaoXuanQuy1985 <cun.kin1985.working@gmail.com>
2020-07-13 20:45:28 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bai-Thi-Thuc-Hanh-Module-Angular';
+}
